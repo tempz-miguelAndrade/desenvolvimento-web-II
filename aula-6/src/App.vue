224a1da1d1aaa2ea04edd4cc8c2591ea9ed4c41d
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-03-09 11:55:57 -0300

    nova aula, nova lista feita com algumas coisas. Falta a function editTarefa

---
+++ aula-6/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+const tarefas = ref(['tarefa 1', 'tarefa 2', 'tarefa 3'])
+const novaTarefa = ref('')
+
+function addTarefa() {
+  if (novaTarefa.value.trim().length >= 5) {
+    tarefas.value.push(novaTarefa.value);
+  }
+  novaTarefa.value = '';
+}
+
+
+function delTarefa(item) {
+  const posicao = tarefas.value.indexOf(item);
+  tarefas.value.splice(posicao, 1);
+}
+
+
+function ordenarTarefas() {
+  tarefas.value.sort();
+}
+
+
+function editarTarefa(item) {
+  const posicao = tarefas.value.indexOf(item);
+  tarefas.value[posicao, 1] = ('');
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Lista de tarefas</h1>
+    <input type="text" v-model="novaTarefa" />
+    <button @click="addTarefa()">Add</button><br />
+    <button @click="ordenarTarefas()">Ordenar</button>
+    <ul>
+      <li v-for="tarefa in tarefas" :key="tarefa">
+        {{ tarefa }}
+        <span>
+          <a href="#" @click.prevent="editTarefa()">editar</a>
+          <a href="#" @click.prevent="delTarefa(tarefa)">Delete</a>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
