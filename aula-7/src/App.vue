0a005919eecb60292c36bc986b1c037f89dc3935
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-03-16 11:58:58 -0300

    Add e del feitos, falta edit, ordenar e concluir

--- aula-7/src/App.vue
+++ aula-7/src/App.vue
@@ -22,17 +22,43 @@ const tarefas = ref([
     status: 'pendente',
   },
 ])
+
+
+
+const novaTarefa = ref('')
+
+function addTarefa(item) {
+  tarefas.value.push({ id: tarefas.value[tarefas.value.length -1].id + 1 , tarefa: item, status: 'pendente' })
+}
+
+
+function delTarefa(item) {
+  let i = tarefas.value.indexOf(item)
+  tarefas.value.splice(i,1)
+}
+
+
 </script>
+
+
+
 
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
+    <input type="text" v-model="novaTarefa" />
+    <button @click="addTarefa(novaTarefa)">Adicionar</button>
+    <button @click="ordenarTarefa(item)">ordenar</button>
     <ul>
       <li v-for="item in tarefas" :key="item.id">
         {{ item.tarefa }}
+        <button @click="delTarefa(item)">Excluir</button>
       </li>
     </ul>
   </div>
 </template>
+
+
+
 
 <style scoped></style>
