1915c5498291842bce5ec06f16180d7da93ced43
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-03-17 08:52:25 -0300

    terminado o código

--- aula-8/src/App.vue
+++ aula-8/src/App.vue
@@ -1,16 +1,31 @@
 <script setup>
-  import { ref, computed } from 'vue'
+import { ref, computed } from 'vue'
 
-  const nome = ref('')
-  const sobrenome = ref('')
+const nome = ref('')
+const sobrenome = ref('')
+const texto = ref('')
 
-  const nomeCompleto = computed(() => {
-    console.log('Calculando nome completo...')
-    nomeCompleto.value = nome.value + sobrenome.value
-    return `${nome.value} ${sobrenome.value}`
-  })
+const nomeCompleto = computed(() => {
+  console.log('Calculando nome completo...')
+  nomeCompleto.value = nome.value + sobrenome.value
+  return `${nome.value} ${sobrenome.value}`
+})
+
+const nomeCompleto2 = () => {
+  console.log(`Calculando nome completo 2...`)
+  return `${nome.value} ${sobrenome.value}`
+}
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <input type="text" v-model="nome" />
+    <input type="text" v-model="sobrenome" />
+    <label>Nome completo: {{ nomeCompleto }}</label>
+    <label>Nome completo 2: {{ nomeCompleto2() }}</label>
+    <input type="text" v-model="texto"/>
+    <label>{{ texto }}</label>
+  </div>
+</template>
 
 <style scoped></style>
