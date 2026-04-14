3abfc731922faf3aed81de7ceeafdd2f51eefb23
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-04-14 08:25:53 -0300

    aplicando a lista com objects

--- lista-obj+componentizacao/src/App.vue
+++ lista-obj+componentizacao/src/App.vue
@@ -1,47 +1,146 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed, watch } from 'vue'
+const tarefas = ref([
+  {
+    id: 1,
+    tarefa: 'Tarefa 1',
+    status: 'concluida',
+  },
+  {
+    id: 2,
+    tarefa: 'Tarefa 2',
+    status: 'concluida',
+  },
+  {
+    id: 3,
+    tarefa: 'Tarefa 3',
+    status: 'pendente',
+  },
+  {
+    id: 4,
+    tarefa: 'Tarefa 4',
+    status: 'pendente',
+  },
+])
+
+const novaTarefa = ref('')
+const posAlterada = ref(null)
+const filtro = ref('')
+
+function addTarefa(item) {
+  if (novaTarefa.value.trim().length >= 5) {
+    if (posAlterada.value == null) {
+      tarefas.value.push({
+        id: tarefas.value[tarefas.value.length - 1].id + 1,
+        tarefa: item,
+        status: 'pendente',
+      })
+    } else {
+      tarefas.value[posAlterada.value].tarefa = novaTarefa.value
+      posAlterada.value = null
+    }
+  }
+  novaTarefa.value = ''
+}
+
+function delTarefa(item) {
+  let i = tarefas.value.indexOf(item)
+  tarefas.value.splice(i, 1)
+}
+
+function editTarefa(item) {
+  posAlterada.value = tarefas.value.indexOf(item)
+  novaTarefa.value = item.tarefa
+}
+
+function concluirTarefa(item) {
+  item.status = item.status === 'concluida' ? 'pendente' : 'concluida'
+}
+
+const tarefasFiltradas = computed(() => {
+  if (filtro.value.trim().length > 0) {
+    return tarefas.value.filter((item) => item.tarefa.includes(filtro.value))
+  } else {
+    return tarefas.value
+  }
+})
+
+const concluidas = ref(tarefasFiltradas.value.filter((item) => item.status == 'concluida').length)
+const pendentes = ref(tarefasFiltradas.value.filter((item) => item.status == 'pendente').length)
+watch(tarefas.value, () => {
+  concluidas.value = tarefasFiltradas.value.filter((item) => item.status == 'concluida').length
+  pendentes.value = tarefasFiltradas.value.filter((item) => item.status == 'pendente').length
+})
+watch(tarefasFiltradas, () => {
+  concluidas.value = tarefasFiltradas.value.filter((item) => item.status == 'concluida').length
+  pendentes.value = tarefasFiltradas.value.filter((item) => item.status == 'pendente').length
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1>Lista de Tarefas</h1>
+    <input type="text" v-model="novaTarefa" placeholder="adicione algo a lista..." class="add"/>
+    <button @click="addTarefa(novaTarefa)" class="add">Adicionar</button>
+    <div class="contador-div">
+      <p>Pendetes: {{ pendentes }} <span> Concluídas: {{ concluidas }}</span></p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <ul>
+      <li
+        v-for="item in tarefasFiltradas"
+        :key="item.id"
+        :class="{ concluida: item.status === 'concluida' }"
+      >
+        {{ item.tarefa }}
+        <button @click="delTarefa(item)" class="exc">Excluir</button>
+        <button @click="editTarefa(item)" class="edit">Editar</button>
+        <button @click="concluirTarefa(item)" class="conc">Concluir</button>
+      </li>
+    </ul>
+    <div class="filtro-div">
+      <h2>Filtro de lista</h2>
+      <input type="text" v-model="filtro" placeholder="digite o que deseja filtrar..." class="filtro-input"/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.concluida {
+  text-decoration: line-through;
+  color: rgb(52, 234, 52);
+}
+li {
+  color: rgb(241, 62, 62);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.container h1 {
+  font-size: 3rem;
+  color: white;
+  margin-bottom: 2vw;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.container h2 {
+  font-size: 1.5rem;
+  color: white;
+}
+.container button.add, input {
+  margin-bottom: 1vw;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.container ul {
+  margin-bottom: 2vw;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container .contador-div span{
+  margin-left: 1vw;
+}
+
+.container .contador-div p {
+  color: white-gray;
+}
+
+.container .contador-div {
+  margin-bottom: 1vw;
 }
 </style>
